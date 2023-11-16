40652953ba2f969d31e21b285bff6014fba8ad87
John Tran <johntrran@Johns-MacBook-Air.local>
2023-11-16 15:17:18 -0500

    refactored midi message logging to helper function

--- midi/midi.js
+++ midi/midi.js
@@ -1,9 +1,23 @@
+function logMidi(message) {
+    let timestamp = Date.now();
+    let date = new Date(timestamp);
+    console.info(
+        date.getFullYear() + "/" +
+        date.getMonth() + "/" +
+        date.getDay() + " " + 
+        date.getHours() + ":" +
+        date.getMinutes() + ":" + 
+        date.getSeconds() + ":" + 
+        date.getMilliseconds() + " midi note value --- " + 
+        message.data[1]
+    );
+}
+
 function onMIDIMessage (message) {  
     let note = 1;
     let keyOnOrOff = 144;
     if (message.data[0] == keyOnOrOff) {
-        console.info(message);
-        console.info(message.data[note]);
+        logMidi(message);
     }
 }
 
