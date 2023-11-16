6febe7924b883e5f7837733a0e62a784eb5cc04a
John Tran <johntrran@Johns-MacBook-Air.local>
2023-11-16 18:35:28 -0500

    added note buffer functionality. currently logs the final buffered notes, there is no server interaction yet

--- midi/midi.js
+++ midi/midi.js
@@ -1,4 +1,17 @@
-function logMidi(message) {
+import { Countdown } from "./countdown.js";
+
+let noteBuffer = new Set();
+let thing = "thing";
+
+let submitChord = () => {
+    console.log("mock send cord to database");
+    console.log(noteBuffer);
+    noteBuffer.clear();
+}
+
+let countdown = new Countdown(submitChord);
+
+let logMidi = (message) => {
     let timestamp = Date.now();
     let date = new Date(timestamp);
     console.info(
@@ -8,21 +21,32 @@ function logMidi(message) {
         date.getHours() + ":" +
         date.getMinutes() + ":" + 
         date.getSeconds() + ":" + 
-        date.getMilliseconds() + " midi note value --- " + 
-        message.data[1]
+        date.getMilliseconds() + " midi data --- " + 
+        message.data[0] + " " + 
+        message.data[1] + " " + 
+        message.data[2]
     );
 }
 
+
+
 function onMIDIMessage (message) {  
-    let note = 1;
-    let keyOnOrOff = 144;
-    if (message.data[0] == keyOnOrOff) {
+    if (message.data[0] == 144 && message.data[2] != 0) {
         logMidi(message);
+        let noteValue = message.data[1];
+        if (countdown.isActive == false) {
+            countdown.startCountdown();
+        } else {
+            countdown.refreshCountdown();
+        }
+        noteBuffer.add(noteValue);
+
     }
 }
 
 // inputs is an iterator. each time there is a midi message, call the onMIDIMessage function 
 function success (midi) {
+    console.info("connected");
     var inputs = midi.inputs.values();
     for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
         input.value.onmidimessage = onMIDIMessage;
@@ -34,6 +58,7 @@ function failure () {
 }
 
 function connectMidi() {
+    console.info("connecting...")
     if (navigator.requestMIDIAccess) {
         navigator.requestMIDIAccess()
             .then(success, failure);
