8905172ba4949aeba9414de37b77b49edc4eedfb
John Tran <johntrran@Johns-MacBook-Air.local>
2023-11-18 09:41:45 -0500

    refined midi.js to remove game logic, which should have been elsewhere. created handler for midi messages, that you can set from outside the midi object

--- midi/midi.js
+++ midi/midi.js
@@ -1,65 +1,53 @@
-import { Countdown } from "./countdown.js";
-
 class MidiConnection {
-    noteBuffer = new Set();
 
-    submitChord = () => {
-        console.log("mock send cord to database");
-        console.log(this.noteBuffer);
-        this.noteBuffer.clear();
-    }
+    // logMidi = (message) => {
+    //     let timestamp = Date.now();
+    //     let date = new Date(timestamp);
+    //     console.info(
+    //         date.getFullYear() + "/" +
+    //         date.getMonth() + "/" +
+    //         date.getDay() + " " + 
+    //         date.getHours() + ":" +
+    //         date.getMinutes() + ":" + 
+    //         date.getSeconds() + ":" + 
+    //         date.getMilliseconds() + " midi data --- " + 
+    //         message.data[0] + " " + 
+    //         message.data[1] + " " + 
+    //         message.data[2]
+    //     );
+    // }
 
-    countDown = new Countdown(this.submitChord);
-
-    logMidi = (message) => {
-        let timestamp = Date.now();
-        let date = new Date(timestamp);
-        console.info(
-            date.getFullYear() + "/" +
-            date.getMonth() + "/" +
-            date.getDay() + " " + 
-            date.getHours() + ":" +
-            date.getMinutes() + ":" + 
-            date.getSeconds() + ":" + 
-            date.getMilliseconds() + " midi data --- " + 
-            message.data[0] + " " + 
-            message.data[1] + " " + 
-            message.data[2]
-        );
-    }
-
-    onMIDIMessage = (message) => {  
-        if (message.data[0] == 144 && message.data[2] != 0) {
-            this.logMidi(message);
-            let noteValue = message.data[1];
-            if (this.countDown.isActive == false) {
-                this.countDown.startCountdown();
-            } else {
-                this.countDown.refreshCountdown();
-            }
-            this.noteBuffer.add(noteValue);
+    onMIDIMessageHandler = (message) => {
+        if (message.data[0] == 144 || message.data[1] == 128) {
+            console.log("default midi message handler. please explicitly set one via setOnMIDIMessageHandler()");
+            console.log(message);
         }
     }
 
-    success = (midi) => {
+    onConnectSuccess = (midi) => {
         console.info("connected");
         var inputs = midi.inputs.values();
         for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
-            input.value.onmidimessage = this.onMIDIMessage;
+            input.value.onmidimessage = this.onMIDIMessageHandler;
         }
     }
     
-    failure = () => {
+    onConnectFailure = () => {
         console.error('No access to your midi devices.');
     }
     
-    connectMidi = () => {
+    connectMidiDevice = () => {
         console.info("connecting...")
         if (navigator.requestMIDIAccess) {
             navigator.requestMIDIAccess()
-                .then(this.success, this.failure);
+                .then(this.onConnectSuccess, this.onConnectFailure);
         }
     }
+
+    setOnMIDIMessageHandler = (newHandler) => {
+        this.onMIDIMessageHandler = newHandler;
+    }
+
 }
 
 export {MidiConnection};
