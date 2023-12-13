57211efd4d8337eb395c41b698d2ffe32b35e59f
John Tran <johntrran@Johns-MacBook-Air.local>
2023-12-13 12:44:05 -0500

    fixed test to be correct. If 2 notes recieved, before countdown finishes - and the countdown isn't finished yet - onChord should not be called

--- test/keyboard_connection/ChordBuffer.test.ts
+++ test/keyboard_connection/ChordBuffer.test.ts
@@ -60,32 +60,37 @@ describe('when single note received, but timer NOT done yet', () => {
 
 })
 
-describe('when 2 notes received before timer finishes, and then timer finishes', () => {
+describe('when 2 notes received before countdown finishes, and then timer finishes', () => {
 
-    let myChord: Set<number>;
+    let myChord: Set<number> = new Set<number>;
     let getChord = (chord: Set<number>): void => {
         myChord = chord;
     }
 
-    it('the 2 notes should be bundled together as one chord', () => {
+    it('there should be NO notes stored, since the buffer flushes upon countdown finish', () => {
         let buffer = new ChordBuffer(getChord);
         relay.addObserver(buffer);
         relay.receiveMidiMessage(new MidiMessage(144, 60, 1));
         relay.receiveMidiMessage(new MidiMessage(144, 62, 1));
         vi.runAllTimers();
-        expect(myChord.has(60) && myChord.has(62)).toBe(true);
+        expect(myChord.size).toBe(0);
     })
+})
+
+
+
+describe('when 2 notes received before timer finishes, and the timer is NOT finished yet', () => {
 
     it('the 2 notes should be bundled together as one chord, when midi message is recieved barely before time limite both times', () => {
-        let buffer = new ChordBuffer(getChord);
+        let buffer = new ChordBuffer(mockOnChordReady);
         relay.addObserver(buffer);
+
         relay.receiveMidiMessage(new MidiMessage(144, 60, 1));
         vi.advanceTimersByTime(buffer.MS_TILL_BUFFER_FLUSH - 1);
 
         relay.receiveMidiMessage(new MidiMessage(144, 62, 1));
         vi.advanceTimersByTime(buffer.MS_TILL_BUFFER_FLUSH - 1);
-        expect(myChord.has(60) && myChord.has(62)).toBe(true);
+
+        expect(mockOnChordReady).toHaveBeenCalledTimes(0);
     })
-
-
 })
