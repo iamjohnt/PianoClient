93303ef15d43b8c7203183da28aeb48bd1c18dfe
John Tran <johntrran@Johns-MacBook-Air.local>
2023-12-12 22:05:14 -0500

    added KeybourdConnection.ts, which will connect all the pieces together

---
+++ src/keyboard_connection/KeyboardConnection.ts
@@ -0,0 +1,43 @@
+import ChordBuffer from "./ChordBuffer"
+import MidiConnection from "./MidiConnection";
+import MidiConnectionRelay from "./MidiMessageRelay";
+
+export default class KeyboardConnection {
+
+    private chordBuffer: ChordBuffer;
+    private midiConnection: MidiConnection;
+    private midiConnectionRelay: MidiConnectionRelay;
+
+    constructor(onChord: (chord: Set<number>) => void) {
+
+        this.chordBuffer = new ChordBuffer(onChord);
+        this.midiConnection = new MidiConnection();
+        this.midiConnectionRelay = new MidiConnectionRelay();
+
+        // connect buffer -> relay -> midi_connection
+        this.midiConnection.setMidiConnectionRelay(this.midiConnectionRelay);
+        this.midiConnectionRelay.addObserver(this.chordBuffer);
+        
+    }
+
+    public connectMidi = () => {
+
+    }
+
+    public disconnectMidi = () => {
+
+    }
+
+    public getMidiDevices = () => {
+        
+    }
+
+    public setOnConnectMidiSuccess = (newHandler: any) => {
+        this.midiConnection.setOnConnectSuccess(newHandler);
+    }
+
+    public setOnConnectMidiFailure = (newHandler: any) => {
+        this.midiConnection.setOnConnectFailure(newHandler);
+    }
+
+}
