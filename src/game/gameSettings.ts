1975771c9e9ddcabc1fdfb40b9618eb81b95f8ff
John Tran <johntrran@Johns-MacBook-Air.local>
2023-12-13 14:10:14 -0500

    created GameSettings + enums

--- src/game/gameSettings.ts
+++ src/game/gameSettings.ts
@@ -1,25 +1,37 @@
+import { KeySigNote, KeySigMode, ChordPool, WhichHands } from "./Enum";
+
 export class GameSettings {
 
-    public keySigNote;
-    public keySigMode;
-    public chordPool;
-    public whichHands;
-    public leftMin;
-    public leftMax;
-    public rightMin;
-    public rightMax;
-    public length;
+    public keySigNote: KeySigNote;
+    public keySigMode: KeySigMode;
+    public chordPool: ChordPool;
+    public whichHands: WhichHands;
+    public leftMin: number;
+    public leftMax: number;
+    public rightMin: number;
+    public rightMax: number;
+    public length: number;
     
-    constructor() {
-        this.keySigNote = 'C';
-        this.keySigMode = 'MAJOR';
-        this.chordPool = 'NOTE';
-        this.whichHands = 'LEFT';
-        this.leftMin =  21;
-        this.leftMax = 44;
-        this.rightMin = 60;
-        this.rightMax = 80;
-        this.length = 25;
+    constructor(
+        note: KeySigNote,
+        mode: KeySigMode,
+        gameMode: ChordPool,
+        hand: WhichHands,
+        lmin: number,
+        lmax: number,
+        rmin: number,
+        rmax: number,
+        len: number
+    ) {
+        this.keySigNote = note;
+        this.keySigMode = mode;
+        this.chordPool = gameMode;
+        this.whichHands = hand;
+        this.leftMin =  lmin;
+        this.leftMax = lmax;
+        this.rightMin = rmin;
+        this.rightMax = rmax;
+        this.length = len;
     }
 
 }
