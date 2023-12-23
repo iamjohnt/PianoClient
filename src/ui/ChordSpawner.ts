8b487f70f13ec351876224259b3666f5508fb783
John Tran <johntrran@Johns-MacBook-Air.local>
2023-12-22 20:18:15 -0500

    added preliminary calsses GameState and ChordSpawner

---
+++ src/ui/ChordSpawner.ts
@@ -0,0 +1,11 @@
+import ChordObservable from "../keyboard_connection/ChordObservable"
+
+export default class ChordSpawner implements ChordObservable {
+
+    onUpdate(chord: Set<number>): void {
+        // draw chord to ui, by hooking into phaser somehow
+    }
+
+
+    
+}
