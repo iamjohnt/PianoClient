c013917603e8bfc7f95a7c168297c7bffe42bde8
John Tran <johntrran@Johns-MacBook-Air.local>
2023-12-24 15:37:36 -0500

    added comments to SheetNote

--- src/music_model/SheetChord.ts
+++ src/music_model/SheetChord.ts
@@ -1,5 +1,15 @@
 import SheetNote from "./SheetNote"
 
+/*
+each note is the offset from the center of a clef
+ex: 
+    -2, 0, 2 on treble clef
+    0 = center, which is B4
+    2 = D
+    -2 = G
+
+this is done so that the UI code can take this, multiply the offset by the pixel distance between notes
+*/
 export default class SheetChord {
 
     private sheetNotes: Set<SheetNote>
