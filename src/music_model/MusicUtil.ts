1115f18a003ae956536b9e6aef2675170fce3468
John Tran <johntrran@Johns-MacBook-Air.local>
2023-12-25 10:28:38 -0500

    overhauled MusicUtil

--- src/music_model/MusicUtil.ts
+++ src/music_model/MusicUtil.ts
@@ -2,66 +2,47 @@ import { KeySigFull } from "./Enums";
 
 export default class MusicUtil {
 
-    public isWhiteKey = (midi_note: number) => {
+    private whiteNotesSet: Set<number>;
+    private whiteNotesArray: Array<number>;
 
-        let A0 = 21;
-        let C8 = 108;
-        let whiteKeys: Set<number> = new Set();
-        let a_minor_interavals: Array<number> = [2,1,2,2,1,2,2]
-
-        let curNote = A0;
-        let curInterval = 0;
-
-        while (curNote <= C8) {
-            whiteKeys.add(curNote);
-            curNote += a_minor_interavals[curInterval];
-            
-            curInterval = (curInterval + 1) % a_minor_interavals.length;
-        }
-
-        return whiteKeys.has(midi_note);
+    constructor() {
+        this.whiteNotesSet = new Set();
+        this.whiteNotesArray = new Array();
+        this.populateWhiteNotesSetAndArray();
     }
 
-    public getWhiteNotes = (): Array<number> => {
-        let rtn = new Array();
-
-        let A0 = 21;
-        let C8 = 108;
-        let whiteKeys: Array<number> = new Array();
-        let a_minor_interavals: Array<number> = [2,1,2,2,1,2,2]
-
-        let curNote = A0;
-        let curInterval = 0;
-
-        while (curNote <= C8) {
-            whiteKeys.push(curNote);
-            curNote += a_minor_interavals[curInterval];
-            
-            curInterval = (curInterval + 1) % a_minor_interavals.length;
-        }
-        return whiteKeys;
+    public isWhiteKey = (midi_note: number) => {
+        return this.whiteNotesSet.has(midi_note);
     }
 
     public isBlackKey = (midi_note: number) => {
+        return !this.whiteNotesSet.has(midi_note);
+    }
+
+    public getWhiteNotes = (): Array<number> => {
+        return this.whiteNotesArray;
+    }
+
+    private populateWhiteNotesSetAndArray = () => {
 
         let A0 = 21;
         let C8 = 108;
-        let whiteKeys: Set<number> = new Set();
-        let a_minor_interavals: Array<number> = [2,1,2,2,1,2,2]
-
+        let a_minor_jumps: Array<number> = [2,1,2,2,1,2,2]
+        
         let curNote = A0;
-        let curInterval = 0;
+        let curJump = 0;
 
+        // start at first piano key A0, and keep jumping up the keys according to the A Minor pattern
+        // at every jump is a white key, so record it
         while (curNote <= C8) {
-            whiteKeys.add(curNote);
-            curNote += a_minor_interavals[curInterval];
-            
-            curInterval = (curInterval + 1) % a_minor_interavals.length;
+            this.whiteNotesArray.push(curNote);
+            this.whiteNotesSet.add(curNote);
+
+            curNote = curNote + a_minor_jumps[curJump];
+
+            curJump = (curJump + 1) % a_minor_jumps.length;
         }
-
-        return !whiteKeys.has(midi_note);
     }
-
 
 
 
