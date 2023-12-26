cee36d86f40bf42e06f074206ce14d623a337d46
John Tran <johntrran@Johns-MacBook-Air.local>
2023-12-26 18:42:47 -0500

    refactored KeySignature accidental and chromatic logic to other classes

---
+++ src/music_model/KeySigAccidentalMap.ts
@@ -0,0 +1,75 @@
+import { Accidental, KeySigMode, KeySigNote } from "./Enums";
+
+export default class KeySigAccidentalMap {
+
+    private keySigMap: Map<string, Accidental>;
+
+    constructor() {
+
+        // add mappings of key signatures to their accidentals
+
+        // natural root major
+        this.keySigMap.set(this.concat(KeySigNote.C, KeySigMode.MAJOR), Accidental.SHARP)
+        this.keySigMap.set(this.concat(KeySigNote.D, KeySigMode.MAJOR), Accidental.SHARP)
+        this.keySigMap.set(this.concat(KeySigNote.E, KeySigMode.MAJOR), Accidental.SHARP)
+        this.keySigMap.set(this.concat(KeySigNote.F, KeySigMode.MAJOR), Accidental.FLAT)
+        this.keySigMap.set(this.concat(KeySigNote.G, KeySigMode.MAJOR), Accidental.SHARP)
+        this.keySigMap.set(this.concat(KeySigNote.A, KeySigMode.MAJOR), Accidental.SHARP)
+        this.keySigMap.set(this.concat(KeySigNote.A, KeySigMode.MAJOR), Accidental.SHARP)
+
+        // Natural Root Minor
+        this.keySigMap.set(this.concat(KeySigNote.C, KeySigMode.MINOR), Accidental.FLAT)
+        this.keySigMap.set(this.concat(KeySigNote.D, KeySigMode.MINOR), Accidental.FLAT)
+        this.keySigMap.set(this.concat(KeySigNote.E, KeySigMode.MINOR), Accidental.SHARP)
+        this.keySigMap.set(this.concat(KeySigNote.F, KeySigMode.MINOR), Accidental.FLAT)
+        this.keySigMap.set(this.concat(KeySigNote.G, KeySigMode.MINOR), Accidental.FLAT)
+        this.keySigMap.set(this.concat(KeySigNote.A, KeySigMode.MINOR), Accidental.FLAT)
+        this.keySigMap.set(this.concat(KeySigNote.B, KeySigMode.MINOR), Accidental.SHARP)
+
+        // Sharp Root Major
+        this.keySigMap.set(this.concat(KeySigNote.C_SHARP, KeySigMode.MAJOR), Accidental.SHARP)
+        this.keySigMap.set(this.concat(KeySigNote.D_SHARP, KeySigMode.MAJOR), Accidental.SHARP)
+        this.keySigMap.set(this.concat(KeySigNote.F_SHARP, KeySigMode.MAJOR), Accidental.SHARP)
+        this.keySigMap.set(this.concat(KeySigNote.G_SHARP, KeySigMode.MAJOR), Accidental.SHARP)
+        this.keySigMap.set(this.concat(KeySigNote.A_SHARP, KeySigMode.MAJOR), Accidental.SHARP)
+
+        // Sharp Root Minor
+        this.keySigMap.set(this.concat(KeySigNote.C_SHARP, KeySigMode.MINOR), Accidental.SHARP)
+        this.keySigMap.set(this.concat(KeySigNote.D_SHARP, KeySigMode.MINOR), Accidental.SHARP)
+        this.keySigMap.set(this.concat(KeySigNote.F_SHARP, KeySigMode.MINOR), Accidental.SHARP)
+        this.keySigMap.set(this.concat(KeySigNote.G_SHARP, KeySigMode.MINOR), Accidental.SHARP)
+        this.keySigMap.set(this.concat(KeySigNote.A_SHARP, KeySigMode.MINOR), Accidental.SHARP)
+
+
+        // Flat Root Major
+        this.keySigMap.set(this.concat(KeySigNote.D_FLAT, KeySigMode.MAJOR), Accidental.FLAT)
+        this.keySigMap.set(this.concat(KeySigNote.E_FLAT, KeySigMode.MAJOR), Accidental.FLAT)
+        this.keySigMap.set(this.concat(KeySigNote.G_FLAT, KeySigMode.MAJOR), Accidental.FLAT)
+        this.keySigMap.set(this.concat(KeySigNote.A_FLAT, KeySigMode.MAJOR), Accidental.FLAT)
+        this.keySigMap.set(this.concat(KeySigNote.B_FLAT, KeySigMode.MAJOR), Accidental.FLAT)
+
+        // Flat Root Minor
+        this.keySigMap.set(this.concat(KeySigNote.D_FLAT, KeySigMode.MINOR), Accidental.FLAT)
+        this.keySigMap.set(this.concat(KeySigNote.E_FLAT, KeySigMode.MINOR), Accidental.FLAT)
+        this.keySigMap.set(this.concat(KeySigNote.G_FLAT, KeySigMode.MINOR), Accidental.FLAT)
+        this.keySigMap.set(this.concat(KeySigNote.A_FLAT, KeySigMode.MINOR), Accidental.FLAT)
+        this.keySigMap.set(this.concat(KeySigNote.B_FLAT, KeySigMode.MINOR), Accidental.FLAT)
+
+    }
+
+    public getAccidental = (note: KeySigNote, mode: KeySigMode): Accidental => {
+        let result = this.keySigMap.get(this.concat(note, mode));
+        if (result == undefined) {
+            console.log("accidental not found, error")
+            return Accidental.NATURAL;
+        } else {
+            return result;
+        }
+    }
+
+    private concat(e1: KeySigNote, e2: KeySigMode): string {
+        return `${KeySigNote[e1]}${KeySigMode[e2]}`;
+    }
+    
+
+}
--- src/music_model/KeySignature.ts
+++ src/music_model/KeySignature.ts
@@ -1,128 +1,33 @@
+import ChromaticNotes from "./ChromaticNotes";
 import { KeySigNote, KeySigMode, Accidental, KeySigFull } from "./Enums";
+import KeySigAccidentalMap from "./KeySigAccidentalMap";
 
 export default class KeySignature {
 
     private note: KeySigNote;
     private mode: KeySigMode;
     private accidental: Accidental;
+    private chromaticNotes: ChromaticNotes;
+
+    private keySigAccMap: KeySigAccidentalMap;
 
     constructor(note: KeySigNote, mode: KeySigMode) {
 
         this.note = note;
         this.mode = mode;
 
-        let fullKeySig = this.getKeySigFull(note, mode);
-        
-        this.accidental = this.determineAccidental(fullKeySig);
+        this.keySigAccMap = new KeySigAccidentalMap();
+        this.accidental = this.keySigAccMap.getAccidental(note, mode);
+
+        this.chromaticNotes = new ChromaticNotes(note, mode);
+
     }
 
-    private determineAccidental = (keySig: KeySigFull): Accidental => {
-
-        let acc: Accidental;
-        
-        switch (keySig) {
-            // Natural Root Major
-            case KeySigFull.C_MAJOR: acc = Accidental.SHARP; break;
-            case KeySigFull.D_MAJOR: acc = Accidental.SHARP; break;
-            case KeySigFull.E_MAJOR: acc = Accidental.SHARP; break;
-            case KeySigFull.F_MAJOR: acc = Accidental.FLAT; break;
-            case KeySigFull.G_MAJOR: acc = Accidental.SHARP; break;
-            case KeySigFull.A_MAJOR: acc = Accidental.SHARP; break;
-            case KeySigFull.B_MAJOR: acc = Accidental.SHARP; break;
-        
-            // Natural Root Minor
-            case KeySigFull.C_MINOR: acc = Accidental.FLAT; break;
-            case KeySigFull.D_MINOR: acc = Accidental.FLAT; break;
-            case KeySigFull.E_MINOR: acc = Accidental.SHARP; break;
-            case KeySigFull.F_MINOR: acc = Accidental.FLAT; break;
-            case KeySigFull.G_MINOR: acc = Accidental.FLAT; break;
-            case KeySigFull.A_MINOR: acc = Accidental.FLAT; break;
-            case KeySigFull.B_MINOR: acc = Accidental.SHARP; break;
-        
-            // Sharp Root Major
-            case KeySigFull.C_SHARP_MAJOR: acc = Accidental.SHARP; break;
-            case KeySigFull.D_SHARP_MAJOR: acc = Accidental.SHARP; break;
-            case KeySigFull.F_SHARP_MAJOR: acc = Accidental.SHARP; break;
-            case KeySigFull.G_SHARP_MAJOR: acc = Accidental.SHARP; break;
-            case KeySigFull.A_SHARP_MAJOR: acc = Accidental.SHARP; break;
-            
-            // Sharp Root Minor
-            case KeySigFull.C_SHARP_MINOR: acc = Accidental.SHARP; break;
-            case KeySigFull.D_SHARP_MINOR: acc = Accidental.SHARP; break;
-            case KeySigFull.F_SHARP_MINOR: acc = Accidental.SHARP; break;
-            case KeySigFull.G_SHARP_MINOR: acc = Accidental.SHARP; break;
-            case KeySigFull.A_SHARP_MINOR: acc = Accidental.SHARP; break;
-
-            // Flat Root Major
-            case KeySigFull.D_FLAT_MAJOR: acc = Accidental.FLAT; break;
-            case KeySigFull.E_FLAT_MAJOR: acc = Accidental.FLAT; break;
-            case KeySigFull.G_FLAT_MAJOR: acc = Accidental.FLAT; break;
-            case KeySigFull.A_FLAT_MAJOR: acc = Accidental.FLAT; break;
-            case KeySigFull.B_FLAT_MAJOR: acc = Accidental.FLAT; break;
-
-            // Flat Root Minor
-            case KeySigFull.D_FLAT_MINOR: acc = Accidental.FLAT; break;
-            case KeySigFull.E_FLAT_MINOR: acc = Accidental.FLAT; break;
-            case KeySigFull.G_FLAT_MINOR: acc = Accidental.FLAT; break;
-            case KeySigFull.A_FLAT_MINOR: acc = Accidental.FLAT; break;
-            case KeySigFull.B_FLAT_MINOR: acc = Accidental.FLAT; break;
-        }
-        return acc;
-    }
-
-    private getKeySigFull = (note: KeySigNote, mode: KeySigMode): KeySigFull => {
-
-        let keySig: KeySigFull;
-
-        if (note == KeySigNote.C && mode == KeySigMode.MAJOR) { keySig = KeySigFull.C_MAJOR }
-        else if (note == KeySigNote.D && mode == KeySigMode.MAJOR) { keySig = KeySigFull.D_MAJOR }
-        else if (note == KeySigNote.E && mode == KeySigMode.MAJOR) { keySig = KeySigFull.E_MAJOR }
-        else if (note == KeySigNote.F && mode == KeySigMode.MAJOR) { keySig = KeySigFull.F_MAJOR }
-        else if (note == KeySigNote.G && mode == KeySigMode.MAJOR) { keySig = KeySigFull.G_MAJOR }
-        else if (note == KeySigNote.A && mode == KeySigMode.MAJOR) { keySig = KeySigFull.A_MAJOR }
-        else if (note == KeySigNote.B && mode == KeySigMode.MAJOR) { keySig = KeySigFull.B_MAJOR }
-
-        // natural root minor
-        else if (note == KeySigNote.C && mode == KeySigMode.MINOR) { keySig = KeySigFull.C_MINOR }
-        else if (note == KeySigNote.D && mode == KeySigMode.MINOR) { keySig = KeySigFull.D_MINOR }
-        else if (note == KeySigNote.E && mode == KeySigMode.MINOR) { keySig = KeySigFull.E_MINOR }
-        else if (note == KeySigNote.F && mode == KeySigMode.MINOR) { keySig = KeySigFull.F_MINOR }
-        else if (note == KeySigNote.G && mode == KeySigMode.MINOR) { keySig = KeySigFull.G_MINOR }
-        else if (note == KeySigNote.A && mode == KeySigMode.MINOR) { keySig = KeySigFull.A_MINOR }
-        else if (note == KeySigNote.B && mode == KeySigMode.MINOR) { keySig = KeySigFull.B_MINOR }
-
-        // sharp root major
-        else if (note == KeySigNote.C_SHARP && mode == KeySigMode.MAJOR) { keySig = KeySigFull.C_SHARP_MAJOR }
-        else if (note == KeySigNote.D_SHARP && mode == KeySigMode.MAJOR) { keySig = KeySigFull.D_SHARP_MAJOR }
-        else if (note == KeySigNote.F_SHARP && mode == KeySigMode.MAJOR) { keySig = KeySigFull.F_SHARP_MAJOR }
-        else if (note == KeySigNote.G_SHARP && mode == KeySigMode.MAJOR) { keySig = KeySigFull.G_SHARP_MAJOR }
-        else if (note == KeySigNote.A_SHARP && mode == KeySigMode.MAJOR) { keySig = KeySigFull.A_SHARP_MAJOR }
-        
-        // sharp root minor
-        else if (note == KeySigNote.C_SHARP && mode == KeySigMode.MINOR) { keySig = KeySigFull.C_SHARP_MINOR }
-        else if (note == KeySigNote.D_SHARP && mode == KeySigMode.MINOR) { keySig = KeySigFull.D_SHARP_MINOR }
-        else if (note == KeySigNote.F_SHARP && mode == KeySigMode.MINOR) { keySig = KeySigFull.F_SHARP_MINOR }
-        else if (note == KeySigNote.G_SHARP && mode == KeySigMode.MINOR) { keySig = KeySigFull.G_SHARP_MINOR }
-        else if (note == KeySigNote.A_SHARP && mode == KeySigMode.MINOR) { keySig = KeySigFull.A_SHARP_MINOR }
-
-        // flat root major
-        else if (note == KeySigNote.D_FLAT && mode == KeySigMode.MAJOR) { keySig = KeySigFull.D_FLAT_MAJOR }
-        else if (note == KeySigNote.E_FLAT && mode == KeySigMode.MAJOR) { keySig = KeySigFull.E_FLAT_MAJOR }
-        else if (note == KeySigNote.G_FLAT && mode == KeySigMode.MAJOR) { keySig = KeySigFull.G_FLAT_MAJOR }
-        else if (note == KeySigNote.A_FLAT && mode == KeySigMode.MAJOR) { keySig = KeySigFull.A_FLAT_MAJOR }
-        else if (note == KeySigNote.B_FLAT && mode == KeySigMode.MAJOR) { keySig = KeySigFull.B_FLAT_MAJOR }
-
-        // flat root minor
-        else if (note == KeySigNote.D_FLAT && mode == KeySigMode.MINOR) { keySig = KeySigFull.D_FLAT_MINOR }
-        else if (note == KeySigNote.E_FLAT && mode == KeySigMode.MINOR) { keySig = KeySigFull.E_FLAT_MINOR }
-        else if (note == KeySigNote.G_FLAT && mode == KeySigMode.MINOR) { keySig = KeySigFull.G_FLAT_MINOR }
-        else if (note == KeySigNote.A_FLAT && mode == KeySigMode.MINOR) { keySig = KeySigFull.A_FLAT_MINOR }
-        else { keySig = KeySigFull.B_FLAT_MINOR } 
-
-        return keySig;
-    }
-
-    public getAccidental = (): Accidental => {
+    public getAccidental = (): Accidental | undefined => {
         return this.accidental;
+    }
+
+    public isChromatic = (midi_note: number): boolean => {
+        return this.chromaticNotes.isChromatic(midi_note);
     }
 }
