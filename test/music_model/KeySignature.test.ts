f914056825e344675051003a0cfc6a3b74bb0635
John Tran <johntrran@Johns-MacBook-Air.local>
2023-12-27 12:14:15 -0500

    added unit tests for KeySignature

--- test/music_model/KeySignature.test.ts
+++ test/music_model/KeySignature.test.ts
@@ -4,18 +4,34 @@ import {KeySigNote, KeySigMode, Accidental} from "../../src/music_model/Enums"
 import KeySignature from "../../src/music_model/KeySignature"
 
 
-describe('when given a key signature', () => {
-    it('c minor is flat', () => {
+describe('c minor', () => {
+    it('accidental = flat', () => {
 
         let k = new KeySignature(KeySigNote.C, KeySigMode.MINOR);
         let acc = k.getAccidental();
         expect(acc).toBe(Accidental.FLAT)
     })
 
-    it('b flat minor is flat', () => {
+    it('64 is NOT chromatic', () => {
+        let k = new KeySignature(KeySigNote.B_FLAT, KeySigMode.MINOR);
+        let res = k.isChromatic(64);
+        expect(res).toBe(false)
+    })
 
+});
+
+
+describe('b flat minor', () => {
+    it('accidental = flat', () => {
         let k = new KeySignature(KeySigNote.B_FLAT, KeySigMode.MINOR);
         let acc = k.getAccidental();
         expect(acc).toBe(Accidental.FLAT)
+    })
+
+
+    it('60 is NOT chromatic', () => {
+        let k = new KeySignature(KeySigNote.B_FLAT, KeySigMode.MINOR);
+        let res = k.isChromatic(59);
+        expect(res).toBe(false)
     })
 });
