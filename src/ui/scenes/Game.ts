b50f63d497dec12e13fedab40b57d79914fe8bc8
John Tran <johntrran@Johns-MacBook-Air.local>
2023-12-29 21:43:52 -0500

    added ability for scene to instantiate invisible notes at all possible locations on staff

--- src/ui/scenes/Game.ts
+++ src/ui/scenes/Game.ts
@@ -1,10 +1,14 @@
 import { WhichHands } from "../../game/Enum";
 import MidiMessage from "../../keyboard_connection/MidiMessage";
+import { NoteOnOff } from "../../music_model/Enums";
+import SheetNote from "../../music_model/SheetNote";
 import GameContext from "./GameContext";
+import { GameObjects } from "phaser";
 
 export default class Game extends Phaser.Scene{
 
     private context: GameContext;
+    private possiblePlayerSprites: Map<number, GameObjects.Sprite>;
 
     constructor() {
         super({ key: 'Game' });
@@ -28,6 +32,9 @@ export default class Game extends Phaser.Scene{
     public create = () => {
         this.add.image(0, 0, 'staff').setOrigin(0,0);
         this.add.image(0, 0, 'clef').setOrigin(0,0);
+
+        this.possiblePlayerSprites = this.populatePlayerNoteSprites();
+
     };
 
     public update = () => {
@@ -35,7 +42,25 @@ export default class Game extends Phaser.Scene{
 
             // let noteEvent: MidiMessage = this.context.noteEventQ.dequeue();
 
-    };
-  }
+        };
+    }
+
+    private populatePlayerNoteSprites = (): Map<number, GameObjects.Sprite> => {
+
+        let sprites: Map<number, GameObjects.Sprite> = new Map<number, GameObjects.Sprite>();
+
+        let staffCenterPos = 700;
+        let intervalDist = 50;
+        let noteCount = 17;
+        let key = -8; // center of 17 is 9. zero out the 9, and you get range of -8 to +8
+
+        for (let i = key; i < 8; i++) {
+            let y = staffCenterPos - (i * intervalDist);
+            let curSprite: GameObjects.Sprite = this.add.sprite(600, y, 'note').setVisible(false)
+            sprites.set(key, curSprite)
+        }
+
+        return sprites;
+    } 
 
 }
