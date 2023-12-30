aa90c2c741195574425073b2db509e329980d21e
John Tran <johntrran@Johns-MacBook-Air.local>
2023-12-29 21:13:59 -0500

    updated GameContext to convert midi messages -> sheet notes

--- src/ui/scenes/GameContext.ts
+++ src/ui/scenes/GameContext.ts
@@ -1,15 +1,41 @@
 import Queue from "../../data_structure/Queue";
+import { WhichHands } from "../../game/Enum";
 import { GameSettings } from "../../game/GameSettings";
 import MidiMessage from "../../keyboard_connection/MidiMessage";
 import MidiObservable from "../../keyboard_connection/MidiObservable";
+import { Clef } from "../../music_model/Enums";
+import MidiToSheetNote from "../../music_model/MidiToSheetNote";
+import SheetNote from "../../music_model/SheetNote";
 
 export default class GameContext implements MidiObservable{
     
     public settings: GameSettings;
-    public noteEventQ: Queue<MidiMessage> = new Queue<MidiMessage>(200);
+    public noteEventQ: Queue<SheetNote>;
+    private converter: MidiToSheetNote;
+
+    constructor(settings: GameSettings) {
+        this.settings = settings;
+        this.noteEventQ = new Queue<SheetNote>(200);
+        this.converter = this.setupConvertor(settings);
+    }
 
     public onUpdate(midiMessage: MidiMessage): void {
-        this.noteEventQ.enqueue(midiMessage);
+        let sheetNote: SheetNote = this.converter.getSheetNote(midiMessage);
+        this.noteEventQ.enqueue(sheetNote);
+    }
+
+    private setupConvertor = (settings: GameSettings): MidiToSheetNote => {
+        let note = this.settings.getKeySigNote();
+        let mode = this.settings.getKeySigMode();
+        let clef!: Clef;
+
+        if (this.settings.getWhichHands() == WhichHands.LEFT) {
+            clef = Clef.BASS_CLEF 
+        } else if (this.settings.getWhichHands() == WhichHands.RIGHT) {
+            clef = Clef.TREBLE_CLEF
+        }
+
+        return new MidiToSheetNote(note, mode, clef);
     }
     
 }
