b5bc1253c38cede0c6187f71165e582e9550cc10
John Tran <johntrran@Johns-MacBook-Air.local>
2023-12-29 21:24:24 -0500

    updated indent 2 to 4

--- src/ui/scenes/Game.ts
+++ src/ui/scenes/Game.ts
@@ -7,33 +7,33 @@ export default class Game extends Phaser.Scene{
     private context: GameContext;
 
     constructor() {
-      super({ key: 'Game' });
+        super({ key: 'Game' });
     }
 
     public init = (context: GameContext) => {
-      this.context = context;
+        this.context = context;
     }
 
     public preload = () => {
-      this.load.image('staff', 'assets/staff.png')
-      this.load.image('note', 'assets/note.png')
+        this.load.image('staff', 'assets/staff.png')
+        this.load.image('note', 'assets/note.png')
 
-      if (this.context.settings.getWhichHands() == WhichHands.LEFT) {
-        this.load.image('clef', 'assets/bass.png')
-      } else {
-        this.load.image('clef', 'assets/treble.png')
-      }
+        if (this.context.settings.getWhichHands() == WhichHands.LEFT) {
+            this.load.image('clef', 'assets/bass.png')
+        } else {
+            this.load.image('clef', 'assets/treble.png')
+        }
     };
     
     public create = () => {
-      this.add.image(0, 0, 'staff').setOrigin(0,0);
-      this.add.image(0, 0, 'clef').setOrigin(0,0);
+        this.add.image(0, 0, 'staff').setOrigin(0,0);
+        this.add.image(0, 0, 'clef').setOrigin(0,0);
     };
 
     public update = () => {
-      if (!this.context.noteEventQ.isEmpty()) {
+        if (!this.context.noteEventQ.isEmpty()) {
 
-          // let noteEvent: MidiMessage = this.context.noteEventQ.dequeue();
+            // let noteEvent: MidiMessage = this.context.noteEventQ.dequeue();
 
     };
   }
