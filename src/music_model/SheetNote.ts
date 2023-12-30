16fb05ec1636cce9d6435e1edd389a279c7eb5a3
John Tran <johntrran@Johns-MacBook-Air.local>
2023-12-29 22:10:48 -0500

    fixed issue where on or off was not present in sheet note

--- src/music_model/SheetNote.ts
+++ src/music_model/SheetNote.ts
@@ -9,6 +9,7 @@ export default class SheetNote {
     constructor(sheet_note: number, accidental: Accidental, onOrOff: NoteOnOff) {
         this.sheetNote = sheet_note;
         this.accidental = accidental;
+        this.onOrOff = onOrOff;
     }
 
     public getSheetNote = () => {
