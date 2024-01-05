46d0abd4dd8abdf62ee3eebc6b706d9011c6a784
John Tran <johntrran@Johns-MacBook-Air.local>
2024-01-05 17:51:41 -0500

    added more stomp response objects

---
+++ src/stomp_connection/response_objects/EndGameResponse.ts
@@ -0,0 +1,5 @@
+export default class EndGameResponse {
+
+    public isGameEndSuccess: boolean;
+    public message: string;
+}
---
+++ src/stomp_connection/response_objects/EndSessionResponse.ts
@@ -0,0 +1,7 @@
+export default class EndSessionResponse {
+
+    public isEndSessionSuccess: boolean;
+    public sessionId: string;
+    public message: string;
+    
+}
