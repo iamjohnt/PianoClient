be590c2fbc4bc07f171f8f50fa7941c5c2223c35
John Tran <johntrran@Johns-MacBook-Air.local>
2024-01-08 17:57:52 -0500

    unite tested SheetNoteYPositions

---
+++ test/phaser/scenes/play/SheetNoteYPositions.test.ts
@@ -0,0 +1,33 @@
+import {afterEach, beforeEach, describe, test, expect, it, vi} from "vitest";
+
+import "../../../../src/phaser/scenes/play/SheetNoteYPositions"
+import SheetNoteYPositions from "../../../../src/phaser/scenes/play/SheetNoteYPositions";
+import SheetNote from "../../../../src/music_model/SheetNote";
+import { Accidental, NoteOnOff } from "../../../../src/music_model/Enums";
+import ObjectPositions from "../../../../src/phaser/ObjectPositions";
+
+
+let yPos: SheetNoteYPositions;
+
+beforeEach(() => {
+    yPos = new SheetNoteYPositions();
+});
+
+afterEach(() => {
+    let yPos = null;
+});
+
+describe('sheet note y positions', () => {
+    
+    it('center note', () => {
+        let note = new SheetNote(0, Accidental.NATURAL, NoteOnOff.ON)
+        let y = yPos.getYPosition(note);
+        expect(y).toBe(ObjectPositions.STAFF_CENTER_Y())
+    })
+
+    it('higher note', () => {
+        let note = new SheetNote(1, Accidental.NATURAL, NoteOnOff.ON)
+        let y = yPos.getYPosition(note);
+        expect(y).toBe(ObjectPositions.STAFF_CENTER_Y() - (ObjectPositions.UNIT() / 2))
+    })
+})
