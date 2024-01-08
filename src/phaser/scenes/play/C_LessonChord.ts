eec5fbe24b4ea8485fcc0af59cd4dc5c1b6242e2
John Tran <johntrran@Johns-MacBook-Air.local>
2024-01-08 17:53:28 -0500

    create SheetNoteYPositions and make LessonChord use it to place notes on the Y plane

--- src/phaser/scenes/play/C_LessonChord.ts
+++ src/phaser/scenes/play/C_LessonChord.ts
@@ -1,26 +1,33 @@
 import { GameObjects } from "phaser";
 import SheetChord from "../../../music_model/SheetChord";
 import ObjectPositions from "../../ObjectPositions";
+import SheetNoteYPositions from "./SheetNoteYPositions";
 
 export default class C_LessonChord extends Phaser.GameObjects.Container {
 
     private notes: Map<number, GameObjects.Sprite>;
+    private sheetNoteYPositions: SheetNoteYPositions;
 
     constructor(scene: Phaser.Scene, x: number, y: number, sheetChord: SheetChord) {
         super(scene, x, y)
+        scene.add.existing(this);
+
+        this.sheetNoteYPositions = new SheetNoteYPositions();
         this.notes = new Map<number, GameObjects.Sprite>;
         this.spawnNotes(sheetChord);
     }
 
     public spawnNotes = (sheetChord: SheetChord) => {
+        this.scene.add.sprite(200, 200, 'note');
         sheetChord.getSheetNotes().forEach( note => {
             let noteSprite: GameObjects.Sprite;
+            let noteY = this.sheetNoteYPositions.getYPosition(note);
             if (this.isNoteWouldCollide(note.getSheetNote())) {
-                noteSprite = this.scene.add.sprite(0 - ObjectPositions.NOTE_COLLIDE_OFFSET(), 0, 'note')
+                noteSprite = this.scene.add.sprite(0 - ObjectPositions.NOTE_COLLIDE_OFFSET(), noteY, 'note')
                 this.notes.set(note.getSheetNote(), noteSprite)
                 this.add(noteSprite);
             } else {
-                noteSprite = this.scene.add.sprite(0, 0, 'note')
+                noteSprite = this.scene.add.sprite(0, noteY, 'note')
                 this.notes.set(note.getSheetNote(), noteSprite)
                 this.add(noteSprite);
             }
