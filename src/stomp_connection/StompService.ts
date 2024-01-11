713eb11990068aad95cb14ded7a89c57a657f85a
John Tran <johntrran@Johns-MacBook-Air.local>
2024-01-10 22:42:24 -0500

    removed unused comments for StompInbound

--- src/stomp_connection/StompService.ts
+++ src/stomp_connection/StompService.ts
@@ -1,12 +1,7 @@
 import { Client } from "@stomp/stompjs";
 import ChordObservable from "../keyboard_connection/ChordObservable"
-import StompConnection from "./StompConnection";
 import StompInbound from "./StompInbound";
 import StompOutbound from "./StompOutbound";
-import HelloResponse from "./response_objects/HelloResponse";
-import StartGameResponse from "./response_objects/StartGameResponse";
-
-// prepare any callbacks prior to connecting - onConnect subscribes for stomp responses, using those callbacks
 
 export default class StompService implements ChordObservable {
 
@@ -35,32 +30,6 @@ export default class StompService implements ChordObservable {
     }
 
     public connectStomp = () => {
-
-        // this.stompClient.onConnect = () => {
-
-        //     this.stompClient.subscribe('/user/queue/hello', (response: any) => {
-        //         console.log("response received from stomp server")
-        //         let hello: HelloResponse = JSON.parse(response.body);
-        //         this.stompIn.onHelloResponseHandler(hello);
-        //     });
-
-        //     this.stompClient.subscribe('/user/queue/chord', (response: any) => {
-        //         console.log("response received from stomp server")
-        //         console.log(JSON.parse(response.body));
-        //         this.stompIn.onChordResponseHandler(response)
-        //     });
-
-        //     this.stompClient.subscribe('/user/queue/settings', (response: any) => {
-        //         console.log(JSON.parse(response.body));
-        //         this.stompIn.onSettingsResponseHandler(response);
-        //     });
-
-        //     this.stompClient.subscribe('/user/queue/startgame', (response: any) => {
-        //         let chordSequence: StartGameResponse = JSON.parse(response.body);
-        //         this.stompIn.onStartGameResponseHandler(chordSequence)
-        //     });
-        // }
-
         this.stompClient.activate();
     }
 
