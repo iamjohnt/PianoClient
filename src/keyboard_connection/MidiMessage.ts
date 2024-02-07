9a3f915c4dc5bb8f94a1542df770c95c225961f3
John Tran <johntrran@Johns-MacBook-Air.local>
2024-02-07 17:02:48 -0500

    allow Midi message to determine Note off in both old or modern method

--- src/keyboard_connection/MidiMessage.ts
+++ src/keyboard_connection/MidiMessage.ts
@@ -12,7 +12,20 @@ export default class MidiMessage {
 
     public isNoteOn = (): boolean => this.status == 144 && this.velocity != 0;
 
-    public isNoteOff = (): boolean => this.velocity == 0;
+    public isNoteOff = (): boolean => {
+        
+        // for sonita's piano which follows midi spec. casio ctk 4000?
+        // http://midi.teragonaudio.com/tech/midispec/noteoff.htm#:~:text=Indicates%20that%20a%20particular%20note,slowly%20fade%20the%20sound%20out.
+        let condition1 = this.status >= 128 && this.status <= 143;
+        let condition2 = this.note >= 0 && this.note <= 127;
+        let condition3 = this.velocity >= 0 && this.velocity <= 127;
+
+        // my piano, which follows modern note off? yamaha p-45
+        // https://www.midi.org/forum/228-writing-midi-software-send-note-off,-or-zero-velocity-note-on
+        let velocityIs0 = this.velocity == 0;
+
+        return (condition1 && condition2 && condition3) || velocityIs0;
+    }
 
     public getNote = (): number => this.note;
 
